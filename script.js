5b84af62c39a8de74524b57aa9b795ec4e4eaffb
Adeelijah <adeelijah2000@gmail.com>
2022-06-28 02:18:43 +0100

    fixed the bugs

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for (var i = 0; themeDots.length > i; i++) {
     themeDots[i].addEventListener('click', function () {
         let mode = this.dataset.mode
-        console.log('Option clicked:', mode)
+        console.log('option clicked:', mode)
         setTheme(mode)
     })
 }
@@ -31,7 +31,7 @@ function setTheme(mode) {
         document.getElementById('theme-style').href = 'green.css'
     }
 
-    if (mode = 'purple') {
+    if (mode == 'purple') {
         document.getElementById('theme-style').href = 'purple.css'
     }
 
