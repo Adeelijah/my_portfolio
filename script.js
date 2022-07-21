1e3f9fd44434a41da051191015e9cb513a5575b3
Adeelijah <adeelijah2000@gmail.com>
2022-07-21 15:18:55 +0100

    added some new styling

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-console.log('its working fine')
+
+
+
+(function () {
+    var CharacterPos = 0;
+    var MsgBuffer = "";
+    var TypeDelay = 100;
+    var NxtMsgDelay = 1000;
+    var MsgIndex = 0;
+    var delay;
+    var MsgArray = ["Hi! I'm Elijah a web developer"];
+
+    function StartTyping() {
+        var id = document.getElementById("typing-text");
+        if (CharacterPos != MsgArray[MsgIndex].length) {
+            MsgBuffer = MsgBuffer + MsgArray[MsgIndex].charAt(CharacterPos);
+            id.value = MsgBuffer + "_";
+            delay = TypeDelay;
+            id.scrollTop = id.scrollHeight;
+        } else {
+            delay = NxtMsgDelay;
+            MsgBuffer = "";
+            CharacterPos = -1;
+            if (MsgIndex != MsgArray.length - 1) {
+                MsgIndex++;
+            } else {
+                MsgIndex = 0;
+            }
+        }
+        CharacterPos++;
+        setTimeout(StartTyping, delay);
+    }
+    StartTyping();
+})();
 
 let theme = localStorage.getItem('theme')
 
